f0466941d719e52bd9ecbc13b61b74687651c3b7
ksn38 <ksn38@yandex.ru>
2024-03-28 14:13:46 +0300

    company guide

---
+++ app/Http/Controllers/CompanyGuideController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Enums\Role;
+use App\Models\User;
+use App\Models\Company;
+use App\Http\Requests\StoreGuideRequest;
+use App\Http\Requests\UpdateGuideRequest;
+use Illuminate\Support\Facades\Gate;
+
+class CompanyGuideController extends Controller
+{
+    public function index(Company $company)
+    {
+        Gate::authorize('viewAny', $company);
+        $guides = $company->users()->where('role_id', Role::GUIDE->value)->get();
+        return view('companies.guides.index', compact('company', 'guides'));
+    }
+
+    public function create(Company $company)
+    {
+        Gate::authorize('create', $company);
+        return view('companies.guides.create', compact('company'));
+    }
+
+    public function store(StoreGuideRequest $request, Company $company)
+    {
+        Gate::authorize('create', $company);
+        $company->users()->create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+            'role_id' => Role::GUIDE->value,
+        ]);
+
+        return to_route('companies.guides.index', $company);
+    }
+
+    public function edit(Company $company, User $guide)
+    {
+        Gate::authorize('update', $company);
+        return view('companies.guides.edit', compact('company', 'guide'));
+    }
+
+    public function update(UpdateGuideRequest $request, Company $company, User $guide)
+    {
+        Gate::authorize('update', $company);
+        $guide->update($request->validated());
+        return to_route('companies.guides.index', $company);
+    }
+
+    public function destroy(Company $company, User $guide)
+    {
+        Gate::authorize('delete', $company);
+        $guide->delete();
+        return to_route('companies.guides.index', $company);
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\CompanyUserController;
+use App\Http\Controllers\CompanyGuideController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +20,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('companies', CompanyController::class)->middleware('isAdmin');
     Route::resource('companies.users', CompanyUserController::class)->except('show');
+    Route::resource('companies.guides', CompanyGuideController::class)->except('show');
 });
 
 require __DIR__.'/auth.php';
